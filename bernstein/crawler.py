0da390b39ddc896d7ddc77f0a79c2aa5da9f1dca
Nathan Jordan <natedagreat27274@gmail.com>
2014-02-09 21:17:40 -0800

    py2neo is working, can insert lots of crawled pages

--- bernstein/crawler.py
+++ bernstein/crawler.py
@@ -11,7 +11,10 @@ from scrapy.spider import Spider
 #import scrapy.contrib.spiders.CrawlSpider as CrawlSpider
 #import scrapy.contrib.spiders.Rule as Rule
 from scrapy.selector import Selector
-#import scrapy.contrib.linkextractors.sgml.SgmlLinkExtractor as SgmlLinkExtractor
+
+import database
+
+urls = ["http://cnn.com/"]
 
 
 class Link(Item):
@@ -24,24 +27,23 @@ class TestSpider(Spider):
 
     name = "test_spider"
     allowed_domains = ["cnn.com"]
-    start_urls = ["http://cnn.com/"]
-
-    #rules = (
-    #    Rule(SgmlLinkExtractor(allow=("*", )), callback="parse_item"),
-    #)
+    start_urls = urls
 
     def parse(self, response):
         sel = Selector(response)
-
         for url in sel.css("a::attr(href)").extract():
             abs_url = urlparse.urljoin(response.url, url.strip())
             o = urlparse.urlparse(abs_url)
+            if not len(o.scheme) or o.scheme is "javascript":
+                continue
             queryless_url = o.scheme + "://" + o.netloc + o.path
-            self.log("Added " + queryless_url)
+            database.map_link(self, response.request.url, queryless_url)
             yield Request(queryless_url, callback=self.parse)
 
 
 if __name__ == "__main__":
+    for url in urls:
+        database.create_initial_node(url)
     spider = TestSpider()
     settings = get_project_settings()
     crawler = Crawler(settings)
@@ -49,5 +51,5 @@ if __name__ == "__main__":
     crawler.configure()
     crawler.crawl(spider)
     crawler.start()
-    log.start(loglevel=log.DEBUG, logfile="lolz.txt")
-    reactor.run() # the script will block here until the spider_closed signal was sent
+    log.start(loglevel=log.DEBUG, logfile="lolz.txt", logstdout=True)
+    reactor.run()
--- bernstein/database.py
+++ bernstein/database.py
@@ -1,7 +1,31 @@
-from py2neo import neo4j, node, rel
+from py2neo import neo4j
 
-db = neo4j.GraphDatabaseService("http://localhost:7474/db/bernstein")
+db = neo4j.GraphDatabaseService("http://localhost:7474/db/data/")
 
 db.create()
 
-print db
+db.clear()
+
+url_index = db.get_or_create_index(neo4j.Node, "url_index")
+
+
+def create_initial_node(url):
+    initial_node_props = {"url": url}
+    db.get_or_create_indexed_node("url_index",
+                                  "url",
+                                  url,
+                                  initial_node_props)
+
+
+def map_link(spider, url_referrer, url_destination):
+    destination_props = {"url": url_destination}
+    destination_node = db.get_or_create_indexed_node("url_index",
+                                                     "url",
+                                                     url_destination,
+                                                     destination_props)
+    referrer_node = db.get_indexed_node("url_index", "url", url_referrer)
+    links = destination_node.match_incoming(start_node=referrer_node,
+                                            rel_type="LINKS_TO")
+    link_list = list(links)
+    if not len(link_list):
+        referrer_node.create_path("LINKS_TO", destination_node)
