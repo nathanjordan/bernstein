32621935ebd826651831d5222f02a9a5d0f164c6
Nathan Jordan <natedagreat27274@gmail.com>
2014-03-05 10:41:41 -0800

    added comments, fixed disallowed url's showing up in the db

--- bernstein/crawler.py
+++ bernstein/crawler.py
@@ -14,7 +14,7 @@ from scrapy.selector import Selector
 
 import database
 
-urls = ["http://cnn.com/"]
+urls = ["http://www.cnn.com/"]
 
 
 class Link(Item):
@@ -31,25 +31,56 @@ class TestSpider(Spider):
 
     def parse(self, response):
         sel = Selector(response)
+        # for each anchor that has a href attribute
         for url in sel.css("a::attr(href)").extract():
+            # parse the url
             abs_url = urlparse.urljoin(response.url, url.strip())
             o = urlparse.urlparse(abs_url)
-            if not len(o.scheme) or o.scheme is "javascript":
-                continue
+            # strip the queries
             queryless_url = o.scheme + "://" + o.netloc + o.path
-            database.map_link(self, response.request.url, queryless_url)
+            # get the domain info to check if we're crawling something we don't
+            # need
+            netloc = o.netloc.lower()
+            # normalize the url
+            queryless_url = queryless_url.lower()
+            # make sure its not a javascript link?
+            if 'javascript' in queryless_url:
+                continue
+            # check to see if its in the allowed domains before we add it
+            allowed = False
+            for domain in self.allowed_domains:
+                # if the domain is in the netloc, we can add it
+                if domain in netloc:
+                    allowed = True
+                    break
+            # otherwise move on to the next link
+            if not allowed:
+                continue
+            # add it to the database
+            database.map_link(self,
+                              response.request.url.lower(),
+                              queryless_url)
+            # crawl the resulting link
             yield Request(queryless_url, callback=self.parse)
 
 
 if __name__ == "__main__":
+    # create initial nodes
     for url in urls:
         database.create_initial_node(url)
+    # create a new spider
     spider = TestSpider()
+    # set the crawler settings
     settings = get_project_settings()
     crawler = Crawler(settings)
+    # tell twisted we want to stop when the spider is done
     crawler.signals.connect(reactor.stop, signal=signals.spider_closed)
+    # configure and set which spider the crawler is using
     crawler.configure()
     crawler.crawl(spider)
+    # start crawling
     crawler.start()
-    log.start(loglevel=log.DEBUG, logfile="lolz.txt", logstdout=True)
+    # log to a file instead of stdout
+    log.start(loglevel=log.DEBUG, logfile="bernstein.log", logstdout=True)
+    # start the twisted event loop
     reactor.run()
--- bernstein/database.py
+++ bernstein/database.py
@@ -1,16 +1,22 @@
 from py2neo import neo4j
 
+# connect to the db
 db = neo4j.GraphDatabaseService("http://localhost:7474/db/data/")
 
+# create the database if its not already there
 db.create()
 
+# clear the existing data (might remove this later)
 db.clear()
 
+# create the url node index
 url_index = db.get_or_create_index(neo4j.Node, "url_index")
 
 
 def create_initial_node(url):
+    # set the node properties (currently just the url)
     initial_node_props = {"url": url}
+    # add the node to the index
     db.get_or_create_indexed_node("url_index",
                                   "url",
                                   url,
@@ -18,14 +24,19 @@ def create_initial_node(url):
 
 
 def map_link(spider, url_referrer, url_destination):
+    # create properties for the destination node
     destination_props = {"url": url_destination}
+    # add the node to the index
     destination_node = db.get_or_create_indexed_node("url_index",
                                                      "url",
                                                      url_destination,
                                                      destination_props)
+    # get the node that referred to this one (it should definitely exist)
     referrer_node = db.get_indexed_node("url_index", "url", url_referrer)
+    # get links between these pages
     links = destination_node.match_incoming(start_node=referrer_node,
                                             rel_type="LINKS_TO")
     link_list = list(links)
+    # if theres not already a link, create one
     if not len(link_list):
         referrer_node.create_path("LINKS_TO", destination_node)
